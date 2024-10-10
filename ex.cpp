de47700d8ac1a808881a55a0f398945a1a11f18b
riastath <sdi2100177@uoa.gr>
2024-10-10 14:33:23 +0300

    pslg class cont.

--- ex.cpp
+++ ex.cpp
@@ -1,40 +1,83 @@
 #include "boost/property_tree/ptree.hpp"
 #include "boost/property_tree/json_parser.hpp"
 #include <string.h>
+#include <sstream>
+#include <typeinfo>
 #include <vector>
 #include <iostream>
-using namespace std;
+#include <CGAL/Constrained_Delaunay_triangulation_2.h>
+#include <CGAL/draw_triangulation_2.h>
+#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+
 namespace pt = boost::property_tree;
 
+typedef CGAL::Exact_predicates_inexact_constructions_kernel K;
+typedef CGAL::Exact_predicates_tag Itag;
+typedef CGAL::Constrained_Delaunay_triangulation_2<K, CGAL::Default, Itag> CDT;
+typedef CDT::Point Point;
+typedef CDT::Edge Edge;
+
 class PSLG {
-    string uid;
+private:
+    std::string uid;
     std::vector<int> bounds;
+    // points are ints 
+    std::vector<int> points_x;
+    std::vector<int> points_y;
+    std::vector<std::pair<Point, Point>> additional_constraints;
+    int num_points;
+    int num_constraints;
 
 public:
-    PSLG(string name) {
+    PSLG(std::string filename) {
         pt::ptree root;
-        pt:read_json(name, root);
-        uid = root.get<string>("instance_uid");
+        pt:read_json(filename, root);
+
+        // Insert .json data into the corresponding class members
+        uid = root.get<std::string>("instance_uid");
+        num_points = root.get<int>("num_points");
+        num_constraints = root.get<int>("num_constraints");
 
         for (pt::ptree::value_type &bound : root.get_child("region_boundary")) {
             bounds.push_back(bound.second.get_value<int>());
         }
+
+        for (pt::ptree::value_type &pointx : root.get_child("points_x")) {
+            points_x.push_back(pointx.second.get_value<int>());
+        }
+        
+        for (pt::ptree::value_type &pointy : root.get_child("points_y")) {
+            points_y.push_back(pointy.second.get_value<int>());
+        }
+
+        // Find out why this doesn't work 
+        for (pt::ptree::value_type &add : root.get_child("additional_constraints")) {
+
+
+            // what type is this ?? 
+            // std::cout << typeid(add.first).name() << std::endl;
+            // std::cout << typeid(add.second).name() << std::endl;
+            // additional_constraints.push_back(std::make_pair(px, py));
+            // additional_constraints.push_back(add.second.get_value<std::pair<Point, Point>>());
+        }
     }
 
     void printer() {
-        cout << "uid is: " << uid << endl;
-        cout << "bounds: " << endl;
+        std::cout << "uid is: " << uid << std::endl;
+        std::cout << "bounds: " << std::endl;
         for (int i = 0; i < bounds.size(); i++) {
             std::cout << bounds.at(i) << " ";
         }
         std::cout << std::endl;
     }
+    
 };
 
 
 
 int main(void) {
-    PSLG * graph = new PSLG("tests/data.json");
+    std::string filename = "tests/data.json";
+    PSLG * graph = new PSLG(filename);
     graph->printer();
 
     return 0;
