c7953ddcf0ac6936e6e1a99fc896613753904101
riastath <sdi2100177@uoa.gr>
2024-10-14 20:13:06 +0300

    functions to compute angle and check if obtuse

--- ex.cpp
+++ ex.cpp
@@ -8,6 +8,8 @@
 #include <CGAL/Constrained_Delaunay_triangulation_2.h>
 #include <CGAL/draw_triangulation_2.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <cmath>
+#include <CGAL/Vector_2.h> // used for angle calculation
 
 namespace pt = boost::property_tree;
 
@@ -16,6 +18,7 @@ typedef CGAL::Exact_predicates_tag Itag;
 typedef CGAL::Constrained_Delaunay_triangulation_2<K, CGAL::Default, Itag> CDT;
 typedef CDT::Point Point;
 typedef CDT::Edge Edge;
+typedef K::Vector_2 Vector;
 
 class PSLG {
 private:
@@ -31,6 +34,7 @@ private:
     int num_constraints;
 
 public:
+    // constructor
     PSLG(std::string filename) {
         pt::ptree root;
         pt:read_json(filename, root);
@@ -74,12 +78,10 @@ public:
 
             // del_constr.push_back(std::make_pair(graph_edge[0], graph_edge[1]));
 
-            // std::cout << typeid(add.second).name() << std::endl;
             std::pair<Point, Point> constr = std::make_pair(point_vec.at(graph_edge[0]), point_vec.at(graph_edge[1]));
             additional_constraints.push_back(constr);
-            // additional_constraints.push_back(std::make_pair(px, py));
-            // additional_constraints.push_back(add.second.get_value<std::pair<Point, Point>>());
         }
+
     }
 
     void printer() {
@@ -106,6 +108,47 @@ public:
             delaunay_instance->insert_constraint(constraint.first, constraint.second);
         }
     }
+
+    // Function to calculate angles using the dot product method -> used to check for non-obtuse triangles later
+    double angle(Point a, Point b, Point c) {
+        // calculating the vectors needed for the dot product formula
+        Vector u = b - a;
+        Vector v = c - a;
+
+        double product = u * v;
+        // |u| and |v|. no length function exists, so using square length and taking its root
+        double ulength = std::sqrt(u.squared_length());
+        double vlength = std::sqrt(v.squared_length());
+
+        double cosine = product/(ulength * vlength);
+    
+        // formula is dot(v,u) = |v| * |u| * cos(angle) 
+        double angle = std::acos(cosine);   // arc cosine
+
+        // check angle's degrees. formula given an angle is degrees = angle * 180/pi
+        double degrees = angle * (180/M_PI);
+
+        return degrees;
+    }
+
+    // Function to check if triangle is non-obtuse by calculating angle degrees, using the function above
+    bool is_obtuse(Point a, Point b, Point c) {
+        double angle_A = angle(a, b, c);
+        double angle_B = angle(c, a, b);
+        double angle_C = angle(b, a, c);
+
+        if (angle_A > 90.0 || angle_B > 90.0 || angle_C > 90.0) {
+            return true;    // triangle is obtuse
+        }
+        return false;
+    }
+
+    // to check for the actual triangulation, modify given a cdt instance 
+    // that iterates with finite_faces methods and checks the vertices, after studying how it works
+    // bool is_obtuse(CDT* instance ) {
+
+    //     return false;
+    // }
 };
 
 
@@ -116,7 +159,20 @@ int main(void) {
 
     CDT cdt;
     graph->delaunay_passer(&cdt);
-    CGAL::draw(cdt);
+    // CGAL::draw(cdt);
+
+    // testing with a triangle that should be obtuse
+    Point a(0, 0);
+    Point b(2, 0);
+    Point c(1, 3);
+    bool res = graph->is_obtuse(a, b, c);
+    std::cout << res << std::endl;
+
+    // this is useful for iterating over faces, so i'm keeping it here
+    // Delaunay::Finite_faces_iterator it;
+    // for (it = dt.finite_faces_begin(); it != dt.finite_faces_end(); it++) {
+    //     std::cout << dt.triangle(it) << std::endl;
+    // }
 
     return 0;
 }
